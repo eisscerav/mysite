fba8f847bec12415aab1a5fce6031d029c6a645f
ffan <ffan@nvidia.com>
2018-02-07 16:43:15 +0800

    add search function

--- family/urls.py
+++ family/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url('^visit_info_page/(?P<visitor_id>[0-9]+)/$', views.visit_info_page, name='visit_info_page'),
     url('^visit_edit_page/(?P<user_id>[0-9]+)/(?P<visit_id>[0-9]+)/$', views.visit_edit_page, name='visit_edit_page'),
     url('^visit_edit_action/$', views.visit_edit_action, name='visit_edit_action'),
+    url('^visit_delete/(?P<user_id>[0-9]+)/(?P<visit_id>[0-9]+)/$', views.visit_delete, name='visit_delete'),
+    url('^user_delete/(?P<user_id>[0-9]+)/$', views.user_delete, name='user_delete'),
+    url('^user_search/$', views.user_search, name='user_search'),
     url('^test_page/$', views.test_page, name='test_page'),
-    url('^car/$', views.car, name='car')
+    url('^car/$', views.car, name='car'),
 ]
--- family/views.py
+++ family/views.py
@@ -25,6 +25,13 @@ def visit_edit_page(request, user_id, visit_id):
     if user_id == '0':
         # return render(request, 'family/visit_edit_page.html')
         return render(request, 'family/visit_edit_page.html', {'user': ''})
+    if visit_id == '0':
+        user = models.User.objects.get(pk=user_id)
+        ctx = {
+            'user': user,
+            'visit_info': ''
+        }
+        return render(request, 'family/visit_edit_page.html', ctx)
     user = models.User.objects.get(pk=user_id)
     visit_info = models.VisitHospital.objects.get(pk=visit_id)
     ctx = {
@@ -43,31 +50,57 @@ def visit_edit_action(request):
     pathogenesis = request.POST.get('pathogenesis')
     user_id = request.POST.get('user_id_hidden')
     visit_id = request.POST.get('visit_id_hidden')
-    if user_id == '':
+    if user_id == u'':
         new_user = models.User.objects.create(name=user_name, age=user_age, gender=user_gender)
-        new_user.save()
         new_user.visithospital_set.create(hospital=hospital, date=date, pathogenesis=pathogenesis)
         new_user.save()
-    else:
+        return render(request, 'family/index.html')
+
+    if user_id != '' and visit_id == '':
         user = models.User.objects.get(pk=user_id)
-        user.name = user_name
-        user.age = user_age
-        user.gender = user_gender
-        visit_info = models.VisitHospital.objects.get(pk=visit_id)
-        visit_info.hospital = hospital
-        visit_info.date = date
-        visit_info.pathogenesis = pathogenesis
+        user.visithospital_set.create(hospital=hospital, date=date, pathogenesis=pathogenesis)
         user.save()
-        visit_info.save()
+        return render(request, 'family/index.html')
+
+    user = models.User.objects.get(pk=user_id)
+    user.name = user_name
+    user.age = user_age
+    user.gender = user_gender
+    visit_info = models.VisitHospital.objects.get(pk=visit_id)
+    visit_info.hospital = hospital
+    visit_info.date = date
+    visit_info.pathogenesis = pathogenesis
+    user.save()
+    visit_info.save()
     return render(request, 'family/index.html')
 
 
-def remove_visit_info(request):
-    pass
+def visit_delete(request, user_id, visit_id):
+    visit_info = models.VisitHospital.objects.get(pk=visit_id)
+    visit_info.delete()
+    return render(request, 'family/index.html')
+
+
+def user_delete(request, user_id):
+    user = models.User.objects.get(pk=user_id)
+    user.delete()
+    return render(request, 'family/index.html')
+
+
+def user_search(request):
+    content = request.GET['q']
+    users = models.User.objects.filter(name__exact=content)
+    context = {
+        'content': content,
+        'users': users
+    }
+    return render(request, 'family/test_page.html', context)
 
 
 def test_page(request):
-    return render(request, 'family/test_page.html')
+    objs = models.TestDatabase.objects.all()
+    context = {'objs': objs}
+    return render(request, 'family/test_page.html', context)
 
 
 def test_edit(request):
